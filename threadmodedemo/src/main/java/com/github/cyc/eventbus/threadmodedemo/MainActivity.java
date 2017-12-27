0ee9e3644d3640e40d8f5404c58fbcfd6232c25e
chongyucaiyan <chongyucaiyan@163.com>
2017-12-27 22:17:18 +0800

    Add thread mode demo

--- threadmodedemo/src/main/java/com/github/cyc/eventbus/threadmodedemo/MainActivity.java
+++ threadmodedemo/src/main/java/com/github/cyc/eventbus/threadmodedemo/MainActivity.java
@@ -1,13 +1,66 @@
 package com.github.cyc.eventbus.threadmodedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initContentView();
+        // 注册订阅者
+        EventBus.getDefault().register(this);
+    }
+
+    private void initContentView() {
+        findViewById(R.id.btn_main_start_activity).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_main_start_activity) {
+            SecondActivity.start(this);
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.POSTING)
+    public void onMessageEventPosting(MessageEvent event) {
+        Log.i(TAG, "onMessageEventPosting(), current thread is " + Thread.currentThread().getName());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEventMain(MessageEvent event) {
+        Log.i(TAG, "onMessageEventMain(), current thread is " + Thread.currentThread().getName());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
+    public void onMessageEventMainOrdered(MessageEvent event) {
+        Log.i(TAG, "onMessageEventMainOrdered(), current thread is " + Thread.currentThread().getName());
+    }
+
+    @Subscribe(threadMode = ThreadMode.BACKGROUND)
+    public void onMessageEventBackground(MessageEvent event) {
+        Log.i(TAG, "onMessageEventBackground(), current thread is " + Thread.currentThread().getName());
+    }
+
+    @Subscribe(threadMode = ThreadMode.ASYNC)
+    public void onMessageEventAsync(MessageEvent event) {
+        Log.i(TAG, "onMessageEventAsync(), current thread is " + Thread.currentThread().getName());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 注销订阅者
+        EventBus.getDefault().unregister(this);
     }
 }
---
+++ threadmodedemo/src/main/java/com/github/cyc/eventbus/threadmodedemo/SecondActivity.java
@@ -0,0 +1,42 @@
+package com.github.cyc.eventbus.threadmodedemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, SecondActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        initContentView();
+    }
+
+    private void initContentView() {
+        findViewById(R.id.btn_second_post_event).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_second_post_event) {
+            // 发布事件
+            new Thread("posting") {
+
+                @Override
+                public void run() {
+                    EventBus.getDefault().post(new MessageEvent("Hello EventBus!"));
+                }
+            }.start();
+        }
+    }
+}
