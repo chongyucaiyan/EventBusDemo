7071c7ca4c4fc8de83acddfe5785b32b3564961d
chongyucaiyan <chongyucaiyan@163.com>
2017-12-27 21:05:20 +0800

    Add basic use demo

---
+++ basicusedemo/src/main/java/com/github/cyc/eventbus/basicusedemo/MessageEvent.java
@@ -0,0 +1,21 @@
+package com.github.cyc.eventbus.basicusedemo;
+
+/**
+ * Created by cyc on 2017/12/27.
+ */
+
+public class MessageEvent {
+    private String message;
+
+    public MessageEvent(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ basicusedemo/src/main/java/com/github/cyc/eventbus/basicusedemo/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.github.cyc.eventbus.basicusedemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, SecondActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        initContentView();
+    }
+
+    private void initContentView() {
+        findViewById(R.id.btn_second_post_event).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_second_post_event) {
+            // 发布事件
+            EventBus.getDefault().post(new MessageEvent("Hello EventBus!"));
+        }
+    }
+}
